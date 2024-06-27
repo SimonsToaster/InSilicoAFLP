baf88f22da39a3d4d5e4920089364a37dca3f81c
SimonsToaster <85318200+SimonsToaster@users.noreply.github.com>
2024-06-27 15:45:47 +0200

    Opens file, started with search for recognition site

--- app.py
+++ app.py
@@ -1,3 +1,33 @@
-#1.Variables
+#1. Variables
 
-#2.Code
+recognition_site = "TTAA"
+cut_index = 1
+
+adaptor_seqence = "GACGATGAGTCCTGAG"
+
+pcr_sequence = "TACTCAGGACTC"
+select_bases = "A"
+
+path_genome = 'Support\ECPlambda.fasta'
+path_resultfile = "Results\savefile"
+
+#2. Code
+#2.1 Imports
+
+import os
+import re 
+
+#2.2 Program 
+
+with open(path_genome, "r") as file:
+    next(file)
+    genome = file.read()
+
+indices = re.search(recognition_site, genome)
+print (indices)
+
+#with open(path_resultfile, "w") as file:
+ #   file.write(genome)
+
+
+
